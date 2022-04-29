bb4e5c48bd3225c110a6b9b294548cda41c9e83b
cmwschroeder <cmwschroeder@gmail.com>
2022-04-29 12:25:13 -0700

    added database schema

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+use movie_db;
+
+CREATE TABLE movies (
+  id INT PRIMARY KEY,
+  movie_name VARCHAR(100)
+);
+
+CREATE TABLE reviews (
+  id INT,
+  movie_id INT,
+  review TEXT NOT NULL,
+  FOREIGN KEY (movie_id)
+  REFERENCES movies(id)
+  ON DELETE SET NULL
+);
